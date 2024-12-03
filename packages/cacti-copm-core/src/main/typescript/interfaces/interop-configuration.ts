e55f3ef81c28e7a13927ee405013b4b751bcf4eb
adrianbatuto <adrian.batuto@accenture.com>
2024-12-03 14:05:01 +0800

    init commit

--- packages/cacti-copm-core/src/main/typescript/interfaces/interop-configuration.ts
+++
@@ -1,7 +0,0 @@
-import { LocalRelayConfig, RemoteNetworkConfig } from "../lib/types";
-
-export interface InteropConfiguration {
-  getLocalRelayConfig(orgKey: string): LocalRelayConfig;
-  getRemoteNetworkConfig(remoteOrgKey: string): RemoteNetworkConfig;
-  interopContractName: string;
-}
